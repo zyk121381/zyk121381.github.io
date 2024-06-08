5a23b71b7ecc93f8c3b7482ac9d6e18cdaa0e499
zyk12138 <598429788@qq.com>
2024-06-09 01:03:15 +0800

    Site updated: 2024-06-09 01:03:15

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/06/06/help/"];function toRandomPost(){
+var posts=["2024/06/06/help/","2024/06/06/project/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
